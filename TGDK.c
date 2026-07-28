a519d4aa9d0e1398e924446098b744b704780bfc
Sean Tichenor aka Willow Tichenor <jtart2133014@gmail.com>
2026-07-28 01:54:14 -0400

    Add threading and memory initialization in main
    Initialize memory lock and create worker threads for corrugation tasks.

--- TGDK.c
+++ TGDK.c
@@ -1,3 +1,5 @@
+
+
 type TGDKHeader struct {
     PrevHash common.Hash
     MobiusHash common.Hash // hash of the entire folded memory state
@@ -14,4 +16,24 @@ func (c *MobiusChain) Seal() {
         c.MobiusHash = crypto.Keccak256Hash(c.Head.Data, c.Tail.Data)
         fmt.Println("TGDK KNOT SEALED:", c.MobiusHash.Hex())
     }
+}
+
+int main() {
+    pthread_mutex_init(&memory_lock, NULL);
+
+    // init 3 phi + 1 pi lattices
+    for(int i=0;i<3;i++) phi_lattice[i].scaling_factor = PHI;
+    pi_lattice[0].scaling_factor = PI;
+
+    corrugation_worker_t workers[4];
+    pthread_t threads[4];
+
+    for(int i=0;i<4;i++) {
+        workers[i].worker_id = i;
+        workers[i].pi_old = 1.0;
+        workers[i].pi_theta = 1.0 + (rand()%40 - 20)/100.0; // simulate learning 0.8-1.2
+        pthread_create(&threads[i], NULL, duo_corrugation_task, &workers[i]);
+    }
+
+    for(int i=0;i<4;i++) pthread_join(threads[i], NULL);
 }
