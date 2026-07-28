99cfc36218c2c5e1c8fb9c03f612d55e10599051
Sean Tichenor aka Willow Tichenor <jtart2133014@gmail.com>
2026-07-28 02:05:25 -0400

    Refactor TGDK.c by moving and correcting includes

--- TGDK.c
+++ TGDK.c
@@ -12,6 +12,28 @@
 #include <unistd.h>
 #include <stdint.h>
 
+#imclude "./core/memory.c"
+#imclude "./core/lattice.c"
+#include "./core/duo.c"
+
+type TGDKHeader struct {
+    PrevHash common.Hash
+    MobiusHash common.Hash // hash of the entire folded memory state
+    Nonce uint64
+    Inverted bool // mobius twist flag
+}
+
+func (c *MobiusChain) Seal() {
+    // When chain length hits 8, 16, 32...
+    // you don't just add a block, you fold the chain through itself
+    // This is your "memory chain folding on top and through itself"
+    if c.Count % 8 == 0 {
+        c.Tail.Inverted = true
+        c.MobiusHash = crypto.Keccak256Hash(c.Head.Data, c.Tail.Data)
+        fmt.Println("TGDK KNOT SEALED:", c.MobiusHash.Hex())
+    }
+}
+
 #define MEMORY_EXPANSION_SNELL 4096
 #define MARA_REALIGNMENT_FACTOR 1.6180339887
 #define PHI 1.6180339887
@@ -203,44 +225,3 @@ int main() {
 
 
 
-#imclude "./core/memory.c"
-#imclude "./core/lattice.c"
-#include "./core/duo.c"
-
-type TGDKHeader struct {
-    PrevHash common.Hash
-    MobiusHash common.Hash // hash of the entire folded memory state
-    Nonce uint64
-    Inverted bool // mobius twist flag
-}
-
-func (c *MobiusChain) Seal() {
-    // When chain length hits 8, 16, 32...
-    // you don't just add a block, you fold the chain through itself
-    // This is your "memory chain folding on top and through itself"
-    if c.Count % 8 == 0 {
-        c.Tail.Inverted = true
-        c.MobiusHash = crypto.Keccak256Hash(c.Head.Data, c.Tail.Data)
-        fmt.Println("TGDK KNOT SEALED:", c.MobiusHash.Hex())
-    }
-}
-
-int main() {
-    pthread_mutex_init(&memory_lock, NULL);
-
-    // init 3 phi + 1 pi lattices
-    for(int i=0;i<3;i++) phi_lattice[i].scaling_factor = PHI;
-    pi_lattice[0].scaling_factor = PI;
-
-    corrugation_worker_t workers[4];
-    pthread_t threads[4];
-
-    for(int i=0;i<4;i++) {
-        workers[i].worker_id = i;
-        workers[i].pi_old = 1.0;
-        workers[i].pi_theta = 1.0 + (rand()%40 - 20)/100.0; // simulate learning 0.8-1.2
-        pthread_create(&threads[i], NULL, duo_corrugation_task, &workers[i]);
-    }
-
-    for(int i=0;i<4;i++) pthread_join(threads[i], NULL);
-}
